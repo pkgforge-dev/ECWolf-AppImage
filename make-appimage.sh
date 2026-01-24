8d6d79a1ab2e37c8828f11343f15f80b0ee36251
Link4Electronics <link4electronics@gmail.com>
2026-01-24 07:53:53 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,17 +3,17 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q d1x-rebirth-git | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q ecwolf | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/pixmaps/d1x-rebirth.xpm
-export DESKTOP=/usr/share/applications/d1x-rebirth.desktop
+export ICON=/usr/share/pixmaps/ecwolf.png
+export DESKTOP=/usr/share/applications/net.maniacsvault.ecwolf.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/d1x-rebirth
+quick-sharun /usr/bin/ecwolf
 
 # Additional changes can be done in between here
 
