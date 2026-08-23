1c5b0fab8d44cb0c19f2d090ff691995ebf1cb55
Link4Electronics <link4electronics@gmail.com>
2026-08-23 20:27:28 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+pacman -Syu --noconfirm cmake sdl2_mixer sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
-# Comment this out if you need an AUR package
-make-aur-package ecwolf
+echo "Building stable version of ECWolf..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/ECWolfEngine/ECWolf"
+VERSION="$(curl -s https://api.github.com/repos/ECWolfEngine/ECWolf/tags | grep '"name"' | grep -v 'pre' | head -1 | cut -d '"' -f 4)"
+git clone --branch "$VERSION" --depth 1 "$REPO" ./ECWolf
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+cmake -S ./ECWolf -B build -DCMAKE_BUILD_TYPE=Release -DGPL=ON
+cmake --build build -j$(nproc)
+mv -v build/ecwolf build/ecwolf.pk3 ./AppDir/bin
