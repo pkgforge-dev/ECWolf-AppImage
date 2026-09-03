d237e67e915648a2bbdf4755db60010ccd614c9a
Link4Electronics <link4electronics@gmail.com>
2026-09-02 23:27:25 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,12 +9,10 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/ECWolfEngine/ECWolf/refs/heads/master/src/macosx/icon.iconset/icon_256x256.png
-export DESKTOP=/usr/share/applications/net.maniacsvault.ecwolf.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/ecwolf
-cp /usr/share/ecwolf/ecwolf.pk3 ./AppDir/bin
+quick-sharun ./AppDir/bin/ecwolf
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Turn AppDir into AppImage
