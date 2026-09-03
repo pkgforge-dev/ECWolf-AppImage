d7a0bf38cf8af40cf9d5c4c67e9d3a4cd6757543
Link4Electronics <link4electronics@gmail.com>
2026-09-02 23:24:50 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -28,6 +28,7 @@ echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd ./ECWolf
+git checkout "$VERSION"
 cmake -S ./ -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_PATCHUTIL=ON \
 		-DGPL=ON
 cmake --build build -j$(nproc)
