21a8ddcee2cc40ac79ac2f6130bf7c89a131bba4
Link4Electronics <link4electronics@gmail.com>
2026-09-02 23:19:56 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,28 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+    cmake      \
+    sdl2_mixer \
+    sdl2_net
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
 # Comment this out if you need an AUR package
-make-aur-package ecwolf
+#make-aur-package ecwolf
 
 # If the application needs to be manually built that has to be done down here
+echo "Building stable version of UEFITool..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/ECWolfEngine/ECWolf"
+VERSION="$(curl -s https://api.github.com/repos/ECWolfEngine/ECWolf/releases/latest | grep '"tag_name"' | cut -d '"' -f 4)"
+git clone "$REPO" ./ECWolf
+echo "$VERSION" > ~/version
+
+mkdir -p ./AppDir/bin
+cd ./ECWolf
+cmake -S ./ -B build -DCMAKE_BUILD_TYPE=Release -DBUILD_PATCHUTIL=ON \
+		-DGPL=ON
+cmake --build build -j$(nproc)
